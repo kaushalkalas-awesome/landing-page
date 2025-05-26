fb958ffdcd05c79c87bfa1052aa315e02b86820f
kaushalkalas-awesome <kaushalkalas8825@gmail.com>
2025-05-26 19:22:01 +0530

    navbar and routing done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'login',
+        component: LoginComponent
+    },
+    {
+        path:'home',
+        component: HomeComponent
+    },
+    {
+        path:'about',
+        component: AboutComponent
+    },
+    {
+        path:'contact',
+        component: ContactComponent
+    },
+    {
+        path:'',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    }
+];
